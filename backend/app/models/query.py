3d63ce9afedfd00dc0a2b8568467028f1f2770e6
OmriLevyTau <omrilevy@mail.tau.ac.il>
2023-05-15 18:07:50 +0300

    added requirments.txt

--- backend/app/models/query.py
+++ backend/app/models/query.py
@@ -5,3 +5,4 @@ from pydantic import BaseModel
 class Query(BaseModel):
     user_id: int
     content: str
+    query_id: int
--- backend/app/routers/documents.py
+++ backend/app/routers/documents.py
@@ -6,15 +6,18 @@ docs_router = APIRouter(
     prefix="/documents"
 )
 
+
 doc_list = [ DocumentMetaData(user_id=1, document_description="desc", 
                               document_title="title", document_id=1, creation_time=dt.datetime.now(),
                               document_size=12) ]
 
 doc_sample = Document(user_id=1, document_id=1, document_title="sample_doc", document_content="content" )
 
+
 @docs_router.get("/")
 async def get_all_docs_metadata() -> list[DocumentMetaData]:
     return doc_list
+
 
 @docs_router.get("/{doc_id}")
 async def get_doc_by_id(doc_id: int) -> Document:
@@ -30,6 +33,7 @@ async def upload_doc(doc: Document) -> DocumentMetaData:
                               document_size=12)
     doc_list.append(doc_metadata)
     return doc_metadata
+
 
 @docs_router.delete("/{doc_id}")
 async def delete_doc(doc_id: int)-> dict: 

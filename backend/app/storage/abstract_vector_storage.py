6de358e31811bafce1842df55db3996757511996
OmriLevyTau <omrilevy@mail.tau.ac.il>
2023-05-16 01:45:51 +0300

    Enhanced project structure and added placeholdes for tests

---
+++ backend/app/storage/abstract_vector_storage.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+
+
+"""
+An abstract class defining basic functionallity each
+vector database provider should implement
+"""
+class AbstractVectorStorage(ABC):
+    pass

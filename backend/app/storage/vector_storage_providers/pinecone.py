5c267803c8ec24e29a82abf0b13e887ff028b6f4
OmriLevyTau <omrilevy@mail.tau.ac.il>
2023-05-21 15:43:43 +0300

    some modifications

--- backend/app/storage/vector_storage_providers/pinecone.py
+++ backend/app/storage/vector_storage_providers/pinecone.py
@@ -3,26 +3,25 @@ from app.storage.abstract_vector_storage import AbstractVectorStorage
 from app.models.documents import (DocumentMetaData, DocumentMetaDataFilter)
 from app.models.query import (QueryResult)
 
+# Note this values should be pre-configured by us.
+# Only thing the client takes care of is maintaining a 
+# dedicated Namespace in whithin this index for
+# each user.
 PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
 PINECONE_ENVIRONMENT = os.environ.get("PINECONE_ENVIRONMENT")
+PINECONE_INDEX = os.environ.get("PINECONE_INDEX")
 
 class PineconeVectorStorage(AbstractVectorStorage):
 
     def __init__(self) -> None:
         self.pinecone_api_key = PINECONE_API_KEY
         self.pinecone_env = PINECONE_API_KEY
+        self.index = PINECONE_INDEX
         
-        if not all([self.pinecone_api_key, self.pinecone_env]):
+        if not all([self.pinecone_api_key, self.pinecone_env, self.index]):
             raise Exception(''' Pinecone connection details not found.
                                 PINECONE_API_KEY and PINECONE_ENVIRONMENT environment varialbes must be set.
                             ''')
-
-        self.index = self._get_index()
-    
-    def _get_index(self):
-        if self.index is not None:
-            return self.index
-        raise NotImplementedError("Initialize an index.")
     
     async def _upload(user_id: str, doc_metadata: DocumentMetaData, vectors: list[float]) -> str:
         raise NotImplementedError("Pinecone")

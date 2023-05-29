34ed68880d23a2ce1178b4e7b71d2044c3b32c4d
TalyaFrancus <talya3228@gmail.com>
2023-05-29 17:23:37 +0300

    Omri save file

--- webui/src/components/common/Table/Table.jsx
+++ webui/src/components/common/Table/Table.jsx
@@ -3,12 +3,15 @@ import { useContext, useState } from "react";
 import { DeleteOutlined, FileTextOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 import { ChatLogContext } from "../../pages/AppContent/ChatContext";
+import { UserContext } from "../../pages/AppContent/AppContext";
+import axios from "axios";
+import Dragger from "antd/es/upload/Dragger";
 
 function FileTable() {
   const navigate = useNavigate();
-  const [file, setFile] = useState();
-  const {dataSource, setDataSource} = useContext(ChatLogContext);
-
+  const [pdfFile, setPdfFile] = useState();
+  const { dataSource, setDataSource } = useContext(ChatLogContext);
+  const { user } = useContext(UserContext);
 
   const columns = [
     {
@@ -62,6 +65,18 @@ function FileTable() {
   };
 
   const UploadFile = (event) => {
+    if (event.file.status !== "uploading") {
+      console.log("Here!");
+      let reader = new FileReader();
+      reader.onload = (e) => {
+        console.log(e.target.result);
+        setPdfFile(e.target.result);
+        console.log("FILEEEEE");
+      };
+      reader.readAsDataURL(event.file.originFileObj);
+    } else {
+      console.log("Error");
+    }
     const uploadedFile = event.file;
     const newFile = {
       name: uploadedFile.name.split(".")[0],
@@ -73,15 +88,60 @@ function FileTable() {
       setDataSource((pre) => [...pre, newFile]);
     }
   };
+  /*
+  const UploadFile = async (event) => {
+    const uploadedFile = event.file;
+    const formData = new FormData();
+    formData.append("file", uploadedFile);
+    formData.append("user_name", user);
+    console.log(formData);
+
+    try {
+      const newFile = {
+        name: uploadedFile.name.split(".")[0],
+        size: `${Math.round(uploadedFile.size / 1024)} KB`,
+        dateModified: new Date().toLocaleDateString(),
+      };
+      const isFileExists = dataSource.some(
+        (file) => file.name === newFile.name
+      );
+      if (!isFileExists) {
+        setDataSource((prev) => [...prev, newFile]);
+      }
+      const response = await axios.post(
+        "http://localhost:8000/gooogle/upload",
+        formData
+      );
+
+      if (response.status === 200) {
+        console.log("File uploaded successfully");
+      } else {
+        console.error("Upload fail");
+      }
+    } catch (error) {
+      console.error("An error occurred during upload", error);
+    }
+  };
+  */
 
   return (
-    <div style={{display: "flex", flexDirection:"column", width:"97%", alignItems:"center"}} >
-        <Upload name="File" onChange={UploadFile} fileList={null} >
-          <Button type="primary">
-            Upload File
-          </Button>
-        </Upload>
-        <Table columns={columns} dataSource={dataSource} style={{paddingTop:"3%", width:"100%"}} rowKey="name" ></Table>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "97%",
+        alignItems: "center",
+      }}
+    >
+      <Dragger name="File" onChange={UploadFile}>
+        <Button type="primary">Upload File</Button>
+      </Dragger>
+      <Table
+        columns={columns}
+        dataSource={dataSource}
+        style={{ paddingTop: "3%", width: "100%" }}
+        rowKey="name"
+      ></Table>
     </div>
   );
 }

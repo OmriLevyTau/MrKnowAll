06e49e8e2e12693fb9e4ce4d6ba8c3987f2ed836
OmriLevyTau <omrilevy@mail.tau.ac.il>
2023-06-13 01:42:17 +0300

    added support for uploading local files and google client extension

--- backend/app/routers/documents.py
+++ backend/app/routers/documents.py
@@ -6,9 +6,9 @@ from fastapi.responses import StreamingResponse
 from app.models.api_models import (GetAllDocumentsMetadataResponse, Status,
                                    UploadDocumentResponse)
 from app.models.documents import Document
-from app.storage.object_storage_providers.google_object_store import (
-    convertDocToPdf, deleteFile, getFileContent, getFileList, uploadFile)
+from app.storage.object_storage_providers.google_object_store import GoogleStorageClient
 from app.storage.vector_storage_providers.pinecone import PineconeVectorStorage
+from app.config import GC_JSON_PATH
 
 docs_router = APIRouter(
     prefix="/api/v0/documents"
@@ -17,12 +17,13 @@ docs_router = APIRouter(
 PDF_PREFIX = 'data:application/pdf;base64,'
 
 pinecone_client = PineconeVectorStorage()
+google_client = GoogleStorageClient(GC_JSON_PATH, "mr-know-all")
 
 
 @docs_router.get("/{user_id}")
 async def get_all_docs_metadata(user_id: str) -> GetAllDocumentsMetadataResponse:
     response = GetAllDocumentsMetadataResponse(
-        status=Status.Ok, docs_metadata=getFileList(user_id))
+        status=Status.Ok, docs_metadata=google_client.get_file_list(user_id))
     return response
 
 
@@ -40,17 +41,24 @@ async def upload_doc(doc: Document) -> UploadDocumentResponse:
     """
     user_id = doc.get_document_metadata().get_user_id()
     doc_id = doc.get_document_metadata().get_document_id()
+
     doc_encoding = doc.pdf_encoding
+    path = doc.path
 
-    if (doc_encoding is not None) and (doc_encoding.startswith(PDF_PREFIX)):
-        doc.pdf_encoding = doc.pdf_encoding[len(PDF_PREFIX):]
+    if (path is None and doc_encoding is None) or (path is not None and doc_encoding is not None):
+        raise ValueError("Document should consist of path or (exclusive) encoding only.")
 
-    path = convertDocToPdf(doc, doc_id)
+    # if doc is defined by encoding
+    if doc_encoding is not None:
+        # make a local (temporary) copy
+        if doc_encoding.startswith(PDF_PREFIX):
+            doc.pdf_encoding = doc.pdf_encoding[len(PDF_PREFIX):]
+        path = google_client.convert_doc_to_pdf(doc, doc_id)
 
     try:
         upload_response = await pinecone_client.upload(user_id=user_id, document=doc)
-        uploadFile(user_id, path, doc_id)
-        os.remove(path)
+        google_client.upload_file(user_id, path, doc_id)
+
         return UploadDocumentResponse(
             status=Status.Ok,
             doc_metadata=doc.get_document_metadata(),
@@ -63,11 +71,17 @@ async def upload_doc(doc: Document) -> UploadDocumentResponse:
             uploaded_vectors_num=0
         )
 
+    finally:
+        if doc_encoding is not None:
+            # it means we've created the temporary file. Hence, we should
+            # clean it as well.
+            os.remove(path)
+
 
 @docs_router.get("/{user_id}/{doc_id}")
 async def get_doc_by_id(user_id: str, doc_id: str):
     # Retrieve the file content from GCS
-    file_content = getFileContent(user_id, doc_id)
+    file_content = google_client.get_file_content(user_id, doc_id)
 
     if file_content is None:
         return Response(status_code=404)
@@ -96,7 +110,7 @@ async def delete_doc(doc_id: str, body: dict) -> dict:
     user_id = body["user_id"]
     try:
         result = await pinecone_client.delete(user_id=user_id, document_id=doc_id)
-        deleteFile(user_id, doc_id)
+        google_client.delete_file(user_id, doc_id)
         if len(result) == 0:
             return {'status': Status.Ok}
         raise Exception('delete failed')
